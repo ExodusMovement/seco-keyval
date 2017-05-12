4953c526b9623f107f04153800424c50694c182b
RyanZim <opensrc@ryanzim.com>
2017-05-12 14:38:30 -0400

    Check if data has changed before writing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import createSecoRW from 'seco-rw'
 import createExpander from 'buffer-noise'
 import { gzipSync, gunzipSync } from 'zlib'
 import fs from 'fs-extra'
+import { createHash } from 'crypto'
 const { expand: expand32k, shrink: shrink32k } = createExpander(2 ** 15)
 
 export default class SecoKeyval {
@@ -10,6 +11,7 @@ export default class SecoKeyval {
     this.file = file
     this.header = header
     this._data = {}
+    this._hash = Buffer.alloc(0)
   }
 
   async open (passphrase, initalData = {}) {
@@ -30,8 +32,13 @@ export default class SecoKeyval {
     if (!this.hasOpened) throw new Error('Must open first.')
     this._data[key] = val
 
-    const data = expand32k(gzipSync(Buffer.from(JSON.stringify(this._data))))
-    await this._seco.write(data)
+    const data = Buffer.from(JSON.stringify(this._data))
+    const hash = createHash('sha256').update(data).digest()
+
+    if (!this._hash.equals(hash)) {
+      this._hash = hash
+      await this._seco.write(expand32k(gzipSync(data)))
+    }
   }
 
   async get (key) {
